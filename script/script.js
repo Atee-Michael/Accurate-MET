d13adb0f657eb8dde205b2c3c24e9ff71a602269
Michael Anthony Atee <104591824+Atee-Michael@users.noreply.github.com>
2023-08-30 16:29:16 +0100

    added apiKey and apiUrl

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+
+// Variable references
+const apiKey = "9d07c9333ee2c5bd9c5167721f6ac08f";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=YOUR_CITY_NAME&units=metric&appid=${apiKey}`;
+
 //get references from input and submit button
 
 const cityInput = document.getElementById("cityInput");
@@ -11,3 +16,9 @@ submitButton.addEventListener("click", function(event){
 
     console.log("City entered is: ", cityName);
 })
+
+
+// get weather data from API
+
+
+// Display weather data
